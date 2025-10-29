e617c64b1dfa3b81ae0b5868b80a11472f7ba384
Malpath <malpath97@gmail.com>
2025-10-29 22:32:24 +0100

    count words

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
         return file_contents
+
 def main ():
-    print(get_book_text("./books/frankenstein.txt"))
-main()
+    return (get_book_text("./books/frankenstein.txt"))
+
+def number_of_words():
+    words = main().split()
+    word_count = len(words)
+    return word_count
+count = number_of_words()
+print(f"Found {count} total words")
 
