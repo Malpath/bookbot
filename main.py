241444b09b9102288da527c32939cdb9bb843d22
Malpath <malpath97@gmail.com>
2025-11-02 21:00:10 +0100

    counted the characters

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+from stats import get_num_words
+from stats import characters_in_text
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
+    
     num_words = get_num_words(text)
     print(f"Found {num_words} total words")
-
+    
+    chars = characters_in_text(text)
+    print(chars)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-from stats import get_num_words
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def characters_in_text(text):
+    characters_dict = {}
+    for ch in text.lower():
+        if ch in characters_dict:
+            characters_dict[ch] += 1
+        else:
+            characters_dict[ch] = 1
+    return characters_dict
