1c6134025939000b94e642d829a77b966054e089
Malpath <malpath97@gmail.com>
2025-11-04 21:45:13 +0100

    arguments done

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import sys
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 from stats import get_num_words
 from stats import characters_in_text
 from stats import sort_characters
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print("============ BOOKBOT ============")
